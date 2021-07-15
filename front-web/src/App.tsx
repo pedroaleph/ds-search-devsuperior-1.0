9e07b490c425aa3d7472c66e857e2f37165890e1
Pedro Aleph <pedro.aleph.allen@gmail.com>
2021-07-15 18:42:38 -0400

    add header component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 
-function App() {
+const App = () => {
   return (
-    <h1>Hello DevSuperior</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
