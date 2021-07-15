7e5237da98169106b5b14f85b96210022ee8396f
Pedro Aleph <pedro.aleph.allen@gmail.com>
2021-07-15 01:41:00 -0400

    Ready for production

--- backend/src/main/java/com/devsuperior/dspesquisa/resources/GenreResource.java
+++
@@ -1,5 +0,0 @@
-package com.devsuperior.dspesquisa.resources;
-
-public class GenreResource {
-
-}
--- backend/src/main/java/com/devsuperior/dspesquisa/resources/RecordResource.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/resources/RecordResource.java
@@ -1,12 +1,18 @@
 package com.devsuperior.dspesquisa.resources;
 
 import java.net.URI;
+import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -27,6 +33,28 @@ public class RecordResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(newDTO.getId()).toUri();
 		return ResponseEntity.created(uri).body(newDTO);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<RecordDTO>> findAll(
+			@RequestParam(value = "min", defaultValue = "") String min,
+			@RequestParam(value = "max", defaultValue = "") String max,
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "size", defaultValue = "0") Integer size,
+			@RequestParam(value = "orderBy", defaultValue = "moment") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+		
+		Instant minDate = ("".equals(min)) ? null : Instant.parse(min);
+		Instant maxDate = ("".equals(max)) ? null : Instant.parse(max);
+		
+		if(size == 0) {
+			size = Integer.MAX_VALUE;
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, size, Direction.valueOf(direction), orderBy);
+		
+		Page<RecordDTO> pageDTO  = service.findByMoments(minDate, maxDate, pageRequest);
+		return ResponseEntity.ok().body(pageDTO);
 	}
 
 }
